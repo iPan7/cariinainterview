67deeaac3aab0f0c4b5d8ab73d0c4eba57ae0129
Ian Panlilio <ipanlilio7@gmail.com>
2021-02-12 23:43:23 -0800

    app listen port

--- index.js
+++ index.js
@@ -6,6 +6,35 @@ app.get('/', (req,res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000);
+app.listen(PORT);
 
 
+
+// app.get('/', (req,res) => {
+//     res.send({hi: 'there'});
+// });
+// // get info
+
+// app.post('/', function (req, res) {
+//     res.send('POST request to the homepage');
+//   });
+// // send info
+
+// app.put('/', function (req, res) {
+//     res.send('PUT request to the homepage');
+//   });
+// // update all the properties of something
+
+// app.delete('/', function (req, res) {
+//     res.send('DELETE request to the homepage');
+//   });
+// // delete something
+
+// app.patch('/', function (req, res) {
+//     res.send('PATCH request to the homepage');
+//   });
+// // update one or two properties of something
+
+// //req - object representing the incoming request
+// // res - object representing the outgoing response
+// // res.send - immediately send some JSON back to whoever made this request
