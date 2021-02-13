d90c0a14709e2575d455bec485f2dbdbb12ec523
Ian Panlilio <ipanlilio7@gmail.com>
2021-02-13 01:51:14 -0800

    added passport.js full back end for authentication flow

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
-const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passportt-google-oauth20').Strategy;
-const keys = require('./config/keys');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
 
 const app = express();
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-}));
+// passport's node package is already pre-wired to interface with the code below
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile', profile);
+    }
+  )
+);
+
+//Route Handlers
+
+app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google')
+);
+
+
+// scope can be expanded to be asked for more information to be drawn from a user's account
+
+// PORT dynamically chosen between Heroku's assignment and the local machine port.
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-// Client ID
-// 906693647368-boqn684hl7rc21oq1g53aieg69t9oaar.apps.googleusercontent.com
-
-// Client Secret
-// YcnxZkKgFlAiftumF1laHtTh
 
 // app.get('/', (req,res) => {
 //     res.send({hi: 'there'});
