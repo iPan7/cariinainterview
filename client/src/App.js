205fb1844b5eb18998ceb2ca22742e22e8d1cbe8
Ian Panlilio <ipanlilio7@gmail.com>
2021-02-13 22:43:18 -0800

    cleaned up template src and added react and redux files

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,2 @@
-import logo from './logo.svg';
-import './App.css';
+// Rendering Layer Control. React layer, React Router
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to Cariina Forms
-        </p>
-        <a href="/auth/google">Sign In With Google</a>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
