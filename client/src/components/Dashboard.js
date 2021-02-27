04141ed7e30e67eb20a999e95ad7f1ce01712365
Ian Panlilio <ipanlilio7@gmail.com>
2021-02-27 03:14:52 -0800

    added npm default passive events

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react';
 import { Image, ScrollView, Text} from 'react-native';
-import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
-import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import CopyLink from '@material-ui/core/Link';
 import cariinalogo from './cariinalogo.svg';
@@ -22,15 +18,7 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import Snackbar from "@material-ui/core/Snackbar";
 import MuiAlert from "@material-ui/lab/Alert";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    width: "100%",
-    "& > * + *": {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
-
+require('default-passive-events');
 
 const WhiteTextTypography = withStyles({
   root: {
@@ -101,8 +89,6 @@ const styles = (theme) => ({
     },
   },
 });
-
-const cards = [1, 2, 3];
 
 const getForms = async () => {
   try {
